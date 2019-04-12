e0c75092427e64e1f4009dfd5362055cf0cfaaf6
Adam Israel <adam.israel@canonical.com>
2019-04-12 11:11:05 -0400

    Initial import

---
+++ README.md
@@ -0,0 +1,24 @@
+# Overview
+
+This is an example of an Open Source Mano (OSM) charm used with the Network Service (ns) example. 
+
+# Usage
+
+
+## Known Limitations and Issues
+
+Network Service charms are an **experimental** feature, expected to be released with OSM R6 in the spring of 2019.
+
+
+# Configuration
+
+None
+
+# Contact Information
+## Open Source Mano (OSM)
+
+  - [OSM website](https://osm.etsi.org/)
+  - [OSM bug tracker](https://osm.etsi.org/bugzilla/)
+  - [OSM_TECH](mailto:OSM_TECH@list.etsi.org) mailing list
+  - [Slack](https://join.slack.com/t/opensourcemano/shared_invite/enQtMzQ3MzYzNTQ0NDIyLWJkMzRjNDM0MjFjODYzMGQ3ODIzMzJlNTg2ZGI5OTdiZjFiNDMyMzYxMjRjNDU4N2FmNjRjNzY5NTE1MjgzOTQ)
+
---
+++ reactive/vnf_policy.py
@@ -0,0 +1,44 @@
+from charmhelpers.core.hookenv import (
+    action_get,
+    action_fail,
+    action_set,
+    status_set,
+    log,
+)
+
+from charms.reactive import (
+    clear_flag,
+    set_flag,
+    when,
+    when_not,
+)
+
+
+@when_not('vnf-policy.installed')
+def install_vnf_b():
+    set_flag('vnf-policy.installed')
+
+
+@when('actions.set-policy')
+def action_set_policy():
+    """Set the policy for a given user.
+
+    Sets the policy (bw and qos) for the specified user_id
+    """
+    err = ''
+    updated = False
+    try:
+        user_id = action_get('user_id')
+        bw = action_get('bw')
+        qos = action_get('qos')
+
+        # If this were a functional vnf, you would perform your operation here
+        # and may return a value to indicate success or failure.
+        updated = True
+
+    except Exception as err:
+        action_fail(str(err))
+    else:
+        action_set({'updated': updated})
+    finally:
+        clear_flag('actions.set-policy')
